395b7e8ff6783b2ce4aae87298e3e8194c471392
avinashpatelin <avinashpatelin@gmail.com>
2021-10-30 13:16:58 +0530

    v0

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+class Welcome{
+
+	public static void main(String[] agrs){
+	
+		System.out.println("Welcome to DevOps world !!!");
+		System.out.println(" Edited first time");
+		System.out.println(" Edited second time");
+
+		System.out.println(" This is to trigger process whenever this code is committed");
+		
+		System.out.println(" Revised V1");
+		
+		System.out.println(" Revised V2");
+	
+	}
+
+}
